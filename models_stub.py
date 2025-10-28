cef21275bc173b7c41371efcf765ab3f66af4e44
DaniyarDaniyar <daniyarkylyshbekov269@gmail.com>
2025-10-28 18:53:57 +0000

    duplicate3:change

--- models_stub.py
+++ models_stub.py
@@ -20,7 +20,15 @@ class User(Model):
 
 def demo():
     User._db.clear()
+<<<<<<< HEAD
     User("Frank", 25)
+=======
+<<<<<<< HEAD
+    User("John", 25)
+=======
+    User("Daniyar", 25)
+>>>>>>> 66f2641 (duplicate3:change)
+>>>>>>> 3316f2b (duplicate3:change)
     User("Bob", 30)
     for u in User.all():
         print(u.id, u.name, u.age)
