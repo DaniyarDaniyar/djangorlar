9389d066ce9ffcd199ab7abcca07eeb3f76608d9
DaniyarDaniyar <daniyarkylyshbekov269@gmail.com>
2025-10-28 19:37:09 +0000

    duplicate3:change

--- service.py
+++ service.py
@@ -18,6 +18,7 @@ class InMemoryStore:
     def add(self, name: str, score: float = 0.0) -> Record:
         record = Record(id=self._next_id, name=name, score=score)
 <<<<<<< HEAD
+<<<<<<< HEAD
         record.updated_at = time.time()
 =======
 <<<<<<< HEAD
@@ -26,6 +27,9 @@ class InMemoryStore:
         record.deleted = False
 >>>>>>> 66f2641 (duplicate3:change)
 >>>>>>> 3316f2b (duplicate3:change)
+=======
+        record.deleted = False
+>>>>>>> 0c707ff (duplicate3:change)
         self._data[self._next_id] = record
         self._next_id += 1
         return record
