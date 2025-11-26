6ffd551d2e4a212306cc8d79fb4b4a86aa92664a
DaniyarDaniyar <daniyarkylyshbekov269@gmail.com>
2025-11-26 21:08:36 +0000

    .

---
+++ settings/conf.py
@@ -0,0 +1,12 @@
+# Project modules
+from decouple import config
+
+#
+#Env id
+#
+ENV_POSSIBLE_OPTIONS = (
+    "local",
+    "prod",
+)
+ENV_ID = config("DJANGORLAR_ENV_ID", cast=str)
+SECRET_KEY = "django-insecure-&1=4@o8+8uj((o8!g973=@(n%334%3r*x(&n0-pc&#ja=er7%e"
